2ea8d79b8432e301e33f2996cc864f409db035a5
sam <samgatete2000@gmail.com>
2025-02-05 16:02:22 +0200

    do while loop

---
+++ src/dowhile.java
@@ -0,0 +1,11 @@
+public class dowhile {
+    public static void main(String[]args){
+        int value = 10;
+
+        do{
+            System.out.println("value is" +value);
+            value +=1;;
+        }
+        while(value < 20);
+    }
+}
---
+++ src/whileloop.java
@@ -0,0 +1,10 @@
+public class whileloop {
+    public static void main(String[]args){
+        int value=10;
+        while(value<20){
+            System.out.println("value is" +value);
+            value +=1;;
+    }
+
+
+    }}
